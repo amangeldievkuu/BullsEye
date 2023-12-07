5ac34a085671512f68bccbec11c3117715dda5c2
Kutman Amangeldiev <kutmanamangeldiev@Kutmans-MacBook-Pro.local>
2023-12-07 16:25:28 +0800

    initial commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Kutman Amangeldiev on 2023/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(){
+        
+    }
+}
+
